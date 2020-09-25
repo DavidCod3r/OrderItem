11e87b575066015bffd3598979c7d5195b16cf6f
David Almeida <davidalmeidadrive@gmail.com>
2020-09-25 00:55:30 -0300

    OrderStatus/Product

---
+++ OrderItem/Entities/Enums/OrderStatus.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderItem.Entities.Enums
+{
+    enum OrderStatus : int
+    {
+        PendingPayment,
+        Processing,
+        Shipped,
+        Delivered
+    }
+}
